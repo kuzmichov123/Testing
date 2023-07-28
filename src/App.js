57bc487e21a7851fccc78dd2924168d487b40e3e
DimaK <dmitry.kuzmichov@gmail.com>
2023-07-28 10:43:09 +0300

    Passed test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+{
+    customer: {
+        firstName: 'Ashley'
+        lastName: 'Jones'
+        phoneNumber: '(123) 555-0123'
+    }
+    stylist: 'Jay Speares'
+    startsAt: '2020-02-02 09:30'
+    service: 'Cut'
+    notes: ''
 }
 
-export default App;
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Appointment", () => {
+  it("renders the customer first name", () => {
+  });
 });
