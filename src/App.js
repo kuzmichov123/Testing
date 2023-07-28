5fe3f2ef79ba9deab94252eea468420d2581b693
DimaK <dmitry.kuzmichov@gmail.com>
2023-07-28 12:06:42 +0300

    Passing test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+export const App = () =>'Ashley'
 {
     customer: {
         firstName: 'Ashley'
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {App} from './App'
+import { act } from "react-dom/test-utils";
+
 describe("Appointment", () => {
   it("renders the customer first name", () => {
-  });
+      const customer = { firstName: 'Ashley' };
+      const component = (
+        <App customer={customer} />
+      );
+      const container = document.createElement("div");
+      document.body.appendChild(container);
+      act(() => 
+        ReactDOM.createRoot(container).render(component)
+      )
+
+      expect(document.body.textContent).toContain(
+        'Ashley'
+      );
+    });
 });
