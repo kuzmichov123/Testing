b8252a1c9320b14292c5e4f25d9e162f7d658d5b
DimaK <dmitry.kuzmichov@gmail.com>
2023-07-28 16:37:29 +0300

    Refactor project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-export const App = ({customer}) => (
+import React from "react";
+
+export const App = ({ customer }) => (
     <div>{customer.firstName}</div>
-)
-
-{
-    customer: {
-        firstName: 'Ashley'
-        lastName: 'Jones'
-        phoneNumber: '(123) 555-0123'
-    }
-    stylist: 'Jay Speares'
-    startsAt: '2020-02-02 09:30'
-    service: 'Cut'
-    notes: ''
-}
-
-
+);
