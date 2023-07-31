3154be9dbf4f87b44597dd826d042dacb8650bfe
DimaK <dmitry.kuzmichov@gmail.com>
2023-07-31 14:55:00 +0300

    Chapter 1-2

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { AppointmentsDayView } from "./Appointment";
+import { sampleAppointments } from "./sampleData";
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+ReactDOM.createRoot(
+    document.getElementById('root')
+).render(
+    <AppointmentsDayView appointments={sampleAppointments}/>
+)
---
+++ src/sampleData.js
@@ -0,0 +1,15 @@
+const today = new Date()
+
+const at = (hours) => today.setHours(hours, 0)
+
+export const sampleAppointments = [
+    { startsAt: at(9), customer: { firstName: 'Charlie'} },
+    { startsAt: at(10), customer: { firstName: 'Vanya'} },
+    { startsAt: at(11), customer: { firstName: 'Anton'} },
+    { startsAt: at(12), customer: { firstName: 'Alexandr'} },
+    { startsAt: at(13), customer: { firstName: 'Ilya'} },
+    { startsAt: at(14), customer: { firstName: 'Victor'} },
+    { startsAt: at(15), customer: { firstName: 'Vasya'} },
+    { startsAt: at(16), customer: { firstName: 'Gleb'} },
+    { startsAt: at(17), customer: { firstName: 'Nigger'} },
+]
