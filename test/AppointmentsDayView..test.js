cdb6cc08152606c27edf76966f4ec2c2c014b707
DimaK <dmitry.kuzmichov@gmail.com>
2023-08-15 13:22:23 +0300

    Cgapter 7 testing hook

--- test/AppointmentsDayView..test.js
+++ test/AppointmentsDayView..test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Appointment, AppointmentsDayView } from '../src/AppointmentsDayView.'
+import { Appointment, AppointmentsDayView } from '../src/AppointmentsDayView'
 import {
     initializeReactContainer,
     render,
--- test/reactTestExtensions.js
+++ test/reactTestExtensions.js
@@ -2,16 +2,17 @@ import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
 
 export let container;
+let reactRoot;
 
 export const initializeReactContainer = () => {
     container = document.createElement("div");
     document.body.replaceChildren(container);
+
+    reactRoot = ReactDOM.createRoot(container);
 };
 
 export const render = (component) =>
-    act(() =>
-        ReactDOM.createRoot(container).render(component)
-    );
+    act(() => reactRoot.render(component));
 
 export const click = (element) =>
     act(() => element.click());
@@ -74,3 +75,6 @@ export const submitButton = () =>
 
 export const labelFor = (formElement) =>
     element(`label[for=${formElement}]`);
+
+export const renderAndWait = (component) => 
+    act(async () => reactRoot.render(component));
